a109ae15259e020485f0db9c05f7944d5b4d9082
Abhijay <abhinn1010@gmail.com>
2025-05-04 14:10:20 +0530

    Fix: Updated app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def register():
             return redirect(url_for('register'))
         
         # Create new user
-        hashed_password = generate_password_hash(password, method='sha256')
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
         new_user = User(username=username, email=email, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
